350ea0a8e5c08f0c363c173834acb70f4722bc53
simras2207 <5492sr@gmail.com>
2022-07-26 13:44:28 +0200

    Finished first part of exercises accordingly to TOP plan

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
 
+  return joinArray;
 };
 
 // Do not edit below this line
